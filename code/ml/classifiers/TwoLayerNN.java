d615a6eb869218466445295873e1c3d3b17c940a
David D'Attile <daviddattile@gmail.com>
2022-03-30 20:14:27 -0700

    update 2

--- code/ml/classifiers/TwoLayerNN.java
+++ code/ml/classifiers/TwoLayerNN.java
@@ -21,16 +21,21 @@ public class TwoLayerNN implements Classifier {
     private int activationFxn;
     private double eta; // aka learning rate
     private double iterations;
+    private boolean includeBias;
 
     // set up weight matrices
-    private double[][] firstLayerWeights;
-    private double[] hiddenLayerWeights; // one-dimensional since we assume 1 output
+    private double[][] hiddenWeights;
+    private double[] outputWeights; // one-dimensional since we assume 1 output
+
+    // make hidden layer values after forward propagation accessible
+    private double[] hiddenLayerValues;
 
     public TwoLayerNN(int numHiddenNodes) {
         this.numHiddenNodes = numHiddenNodes;
         this.eta = 0.1;
         this.iterations = 200;
         this.activationFxn = TANH_ACTIVATION;
+        this.includeBias = true;
     }
 
     /**
@@ -41,21 +46,105 @@ public class TwoLayerNN implements Classifier {
     @Override
     @SuppressWarnings("unchecked")
     public void train(DataSet data) {
-        // TODO: implement bias
+        // determine whether we need to generate a dataset with bias feature
+        DataSet dataToTrain = this.includeBias ? data.getCopyWithBias() : data;
 
-        // init network weights
-        // TODO: maybe bias here?
-        int featureCount = data.getFeatureMap().size();
-        this.initializeWeights(featureCount);
+        // init network weights and hidden values
+        int featureCount = dataToTrain.getFeatureMap().size();
+        initializeWeights(featureCount);
+        initializeHiddenValues();
 
         // get data
-        ArrayList<Example> training = (ArrayList<Example>)data.getData().clone();
+        ArrayList<Example> training = (ArrayList<Example>)dataToTrain.getData().clone();
 
         // loop through iterations
         for (int i = 0; i < this.iterations; i++) {
             // shuffle data
             Collections.shuffle(training);
+
+            // loop through examples
+            for (Example e : training) {
+                // compute example e through the network
+                // saves newly computed hidden layer values
+                double result = forwardCompute(e);
+
+                // backpropagation
+
+            }
+
+            // make some array to hold first layer computations + bias
+
+            // activationFxn(sum(vali * weighti)) --> sum of all vals * weights from one node
         }
+    }
+
+    /**
+     * Initialize the network's weights based on the number of features in an example.
+     * Each weight will be randomized between MIN_RANDOM_WEIGHT and MAX_RANDOM_WEIGHT.
+     *
+     * @param featureCount
+     */
+    private void initializeWeights(int featureCount) {
+        // init bias value
+        int biasValue = this.includeBias ? this.numHiddenNodes + 1 : this.numHiddenNodes;
+
+        // d (num. hidden nodes) * m (feature count) matrix
+        this.hiddenWeights = new double[this.numHiddenNodes][featureCount];
+
+        // d (num. hidden nodes) * o (num. outputs = 1) matrix; +1 for bias weight
+        this.outputWeights = new double[biasValue];
+
+        // init random weight values in hidden layer weights
+        for (double[] currRow : this.hiddenWeights) {
+            for (int i = 0; i < currRow.length; i++) {
+                currRow[i] = getRandomWeightValue();
+            }
+        }
+
+        // init random weight values in output layer weights
+        for (int i =0; i < this.outputWeights.length; i++) {
+            this.outputWeights[i] = getRandomWeightValue();
+        }
+    }
+
+    /**
+     * Initialize the network's hidden node values based on the desired number
+     * of hidden nodes. Include a hard-coded bias of 1 if specified.
+     */
+    private void initializeHiddenValues() {
+        // init bias value based on number of hidden nodes, +1 if true
+        int biasValue = this.includeBias ? this.numHiddenNodes + 1 : this.numHiddenNodes;
+
+        // init hidden layer value array to all 0's
+        this.hiddenLayerValues = new double[biasValue];
+
+        // if using bias, set final array value to 1
+        if (this.includeBias) {
+            this.hiddenLayerValues[this.hiddenLayerValues.length - 1] = 1.0;
+        }
+    }
+
+    /**
+     * Perform the forward computation of an example through the NN.
+     *
+     * @param e
+     * @return a double representing the forward computation result
+     */
+    private double forwardCompute(Example e) {
+        // get feature array from example
+        double[] featureValues = getFeatureArray(e);
+
+        // first layer calculation loop
+        for (int i = 0; i < this.hiddenLayerValues.length; i++) {
+            // get all feature weights for a first layer node d
+            double[] hiddenWeights = this.hiddenWeights[i];
+
+            // save activation of dot product calc (feature array . hidden layer weight array)
+            this.hiddenLayerValues[i] = calculateActivation(dotProduct(featureValues, hiddenWeights));
+        }
+
+        // hidden layer dot product calc (hidden layer vals . output layer weight array)
+        return calculateActivation(dotProduct(this.hiddenLayerValues,this.outputWeights));
     }
 
     /**
@@ -66,12 +155,21 @@ public class TwoLayerNN implements Classifier {
      */
     @Override
     public double classify(Example example) {
-        return 0;
+        // TODO: sigmoid 0 instead of -1.0
+        return forwardCompute(example) > 0 ? 1.0 : -1.0;
     }
 
+    /**
+     * Gives the NN's confidence in its prediction for a given example.
+     * Here, confidence is the absolute value of the result of forward-computing an example
+     * through the network.
+     *
+     * @param example
+     * @return a double representing the NN's confidence
+     */
     @Override
     public double confidence(Example example) {
-        return 0;
+        return Math.abs(forwardCompute(example));
     }
 
     /**
@@ -85,7 +183,7 @@ public class TwoLayerNN implements Classifier {
         if (this.activationFxn == TANH_ACTIVATION) {
             return Math.tanh(input);
         } else { // sigmoid activation
-            return 1 / (1 + Math.exp(-1 * input)); // a?
+            return 1 / (1 + Math.exp(-1 * input));
         }
     }
 
@@ -122,6 +220,13 @@ public class TwoLayerNN implements Classifier {
     }
 
     /**
+     * A helper function for setting whether the network should include a bias in training.
+     *
+     * @param includeBias
+     */
+    public void setIncludeBias(boolean includeBias) { this.includeBias = includeBias; }
+
+    /**
      * An internal helper function for checking if the NN's activation function is tanh.
      */
     private boolean isTanhActivation() {
@@ -136,29 +241,47 @@ public class TwoLayerNN implements Classifier {
     }
 
     /**
-     * Initialize the network's weights based on the number of features in an example.
-     * Each weight will be randomized between MIN_RANDOM_WEIGHT and MAX_RANDOM_WEIGHT.
+     * A helper fxn for calculating a dot product between two arrays.
      *
-     * @param featureCount
+     * @param a1
+     * @param a2
+     * @return a double representing the dot product
      */
-    private void initializeWeights(int featureCount) {
-        // m (feature count) * d (num. hidden nodes) matrix
-        this.firstLayerWeights = new double[featureCount][this.numHiddenNodes];
-
-        // d (num. hidden nodes) * o (num. outputs = 1) matrix
-        this.hiddenLayerWeights = new double[this.numHiddenNodes];
-
-        // init random weight values in first layer weights
-        for (double[] currRow : this.firstLayerWeights) {
-            for (int i = 0; i < currRow.length; i++) {
-                currRow[i] = getRandomWeightValue();
-            }
+    private static double dotProduct(double[] a1, double[] a2) {
+        // if lengths un equal, throw exception
+        if (a1.length != a2.length) {
+            String msg = String.format("Cannot calculate dot product for arrays of different lengths. a1:%d a2:%d", a1.length, a2.length);
+            throw new IllegalArgumentException(msg);
         }
 
-        // init random weight values in hidden layer weights
-        for (int i =0; i < this.hiddenLayerWeights.length; i++) {
-            this.hiddenLayerWeights[i] = getRandomWeightValue();
+        // loop through vals and calc dot product
+        double dp = 0.0;
+        for (int i = 0; i < a1.length; i++) {
+            dp += a1[i] * a2[i];
         }
+
+        return dp;
+    }
+
+    /**
+     * Generate an array of doubles representing an example's feature values
+     *
+     * @param e
+     * @return a double array containing an examples features
+     */
+    private static double[] getFeatureArray(Example e) {
+        // get feature count
+        int featureCount = e.getFeatureSet().size();
+
+        // init feature array
+        double[] featureArray = new double[featureCount];
+
+        // set array vals
+        for (int i = 0; i < featureCount; i++) {
+            featureArray[i] = e.getFeature(i);
+        }
+
+        return featureArray;
     }
 
     /**
@@ -170,5 +293,29 @@ public class TwoLayerNN implements Classifier {
     private static double getRandomWeightValue() {
         // return random value between min and max
         return (Math.random() * ((MAX_RANDOM_WEIGHT - MIN_RANDOM_WEIGHT) + 1)) + MIN_RANDOM_WEIGHT;
+    }
+
+    /**
+     * A helper fxn for nicely printing a double array.
+     *
+     * @param array
+     */
+    private static void printArray(double[] array) {
+        // init string builder
+        StringBuilder s = new StringBuilder("[ ");
+
+        // append values to string
+        for (int i = 0; i < array.length; i++) {
+            double val = array[i];
+
+            // edge case for last val
+            if (i < array.length - 1) {
+                s.append(String.format("%f, ", val));
+            } else {
+                s.append(String.format("%f", val));
+            }
+        }
+
+        System.out.println(s.append(" ]"));
     }
 }
